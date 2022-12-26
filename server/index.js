8d20e0599a382d45a5bf91d7a7fadb02f901fa95
Melissa Imai <melissayuri_903@hotmail.com>
2022-12-26 18:18:00 +0100

    done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express();
 const phones = require("./data/phones.json")
+const cors = require('cors');
+
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 
 app.get("/", (req, res) => console.log("oi"))
 
-app.listen("3000", () => console.log("servidor ligado"))
+app.listen("5000", () => console.log("servidor ligado"))
 
 app.get("/phones", (req, res) => {
   res.json(phones)
